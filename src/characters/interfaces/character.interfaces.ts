d2718f93866372e7aed9d1afc0276a9e44804a46
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-01-19 17:35:27 +0100

    feat: preparado de las interfaces de la respuesta de la api

---
+++ src/characters/interfaces/character.interfaces.ts
@@ -0,0 +1,10 @@
+
+//interfaz preparada para su utilización
+export interface Character {
+    id: number;
+    name: string;
+    image: string;
+    status: string;
+    species: string;
+    gender: string;
+}
---
+++ src/characters/interfaces/rick-and-morty.response.ts
@@ -0,0 +1,62 @@
+export interface TopLevel {
+    info:    Info;
+    results: Result[];
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next:  string;
+    prev:  null;
+}
+
+export interface Result {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   Gender;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+
+/*
+
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Unknown = "unknown",
+}
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}
+
+
+*/
+//Lo he tenigo que cambiar a así porque de la otra manera me daba error
+export type Gender = "Female" | "Male" | "Genderless" | "unknown";
+export type Status = "Alive" | "Dead" | "unknown";
+export type Species = "Alien" | "Human";
+
+
+
+
+export interface Location {
+    name: string;
+    url:  string;
+}
+
+
