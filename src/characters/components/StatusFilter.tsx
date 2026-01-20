5c23cc8a70407ae581f846238462648c65a74baf
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-01-20 21:43:45 +0100

    wip: problemas realizando las busquedas por filtro
    hay un problema por tal como he hecho el codigo que cuando intento filtrar por status carga del cache el anterior valor, me he dado cuenta que repito mucho código  en useCharacter por lo que voy a tratar simplificarlo

--- src/characters/components/StatusFilter.tsx
+++ src/characters/components/StatusFilter.tsx
@@ -6,7 +6,11 @@ interface Props{
 
 export const StatusFilter = ({onFilterChange}: Props) =>{
     //primero me aseguro que cargue bien el componente
-    
+    //detectar que ha cambiado el input (el filtro)
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+        onFilterChange(event.target.value);
+    }
+
     return (
         <div className="status-filter">
             {/* Para poder limpiar el filtro ya que uso radiobuttons */}
@@ -15,24 +19,28 @@ export const StatusFilter = ({onFilterChange}: Props) =>{
                     name="status"
                     value=""
                     defaultChecked
+                    onChange={handleFilterChange}
                 />Todos
             </label>
             <label>
                 <input type="radio"
                     name="status"
                     value="alive"
+                    onChange={handleFilterChange}
                 />Vivo
             </label>
             <label>
                 <input type="radio"
                     name="status"
                     value="dead"
+                    onChange={handleFilterChange}
                 />Muerto
             </label>
             <label>
                 <input type="radio"
                     name="status"
                     value="unknown"
+                    onChange={handleFilterChange}
                 />Desconocido
             </label>
         </div>
