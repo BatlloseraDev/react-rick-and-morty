1c49954a286ebc88451ac3a249d548b04cb31c1d
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-01-20 20:55:11 +0100

    tt: persistencia en cache creada

--- src/characters/hooks/useCharacters.tsx
+++ src/characters/hooks/useCharacters.tsx
@@ -13,6 +13,11 @@ export const useCharacters = () => {
 
     const currentQuery = useRef<string>("");
 
+    //Con useRef mantenemos la referencia entre renderizados (no se pierde al hacer rerender)
+    //   const gifsCache = useRef<Record<string, Gif[]>>({});
+    const charactersCache = useRef<Record<string, Character[]>>({});
+
+
 
     //de momento solo voy a manejar la busqueda normal
     const handleSearch = async (query: string = "") => {
@@ -21,8 +26,19 @@ export const useCharacters = () => {
         setHasError(false);
         currentQuery.current = query.trim().toLowerCase();
         setPage(1);
+
+        if(charactersCache.current[currentQuery.current]){
+            console.log(`He conseguido usar los datos de la cahe: ${currentQuery.current}`);
+            setCharacters(charactersCache.current[currentQuery.current]);
+            setHasError(false);
+            setIsLoading(false);
+            return;
+        }
+
         try {
             const newCharacters = await getCharactersByQuery(currentQuery.current, 1);
+            //si los he conseguido los guardo en la cache
+            charactersCache.current[currentQuery.current] = newCharacters;
             setCharacters(newCharacters);
 
         }//al crear el currentQuery ya no me hace falta hacer la distinción que hacia antes
@@ -46,10 +62,20 @@ export const useCharacters = () => {
     const handleLoadMore = async () => {
         const nextPage = page + 1;
 
+        if(charactersCache.current[currentQuery.current+nextPage]){
+            console.log(`He conseguido usar los datos de la cahe en load more: ${currentQuery.current} - pagina: ${nextPage}`);
+            const newCharacters = charactersCache.current[(currentQuery.current+nextPage)];
+            setCharacters(newCharacters); // no hago [...characters, ...newCharacters] porque en este caso se guardó toda la lista entera
+            setPage(nextPage);
+            return;
+        }
+
         try {
             const newCharacters = await getCharactersByQuery(currentQuery.current, nextPage);
-            setCharacters([...characters, ...newCharacters]);// añado los nuevo resultado a los anteriores
+            const allCharacters = [...characters, ...newCharacters]; //lo modularizo para ser más eficiente
+            setCharacters(allCharacters);// añado los nuevo resultado a los anteriores
             setPage(nextPage);
+            charactersCache.current[currentQuery.current+nextPage] = allCharacters; //aactualmente me estaba dando error por que la key volvía a repetirse pero al añadirle la pagina a la key se vuelve unico y no da problemas
         }
         catch (error) {
             console.log("No hay más páginas o error mu gordo: ", error);
